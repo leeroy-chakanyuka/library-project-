131166e3907da4703e2794b23b2f9524bdb2c9a6
Leeroy Chakanyuka <chakanyukaleeroy62@gmail.com>
2024-09-16 10:18:18 +0200

    finished book class - for now

--- src/library/project/Book.java
+++ src/library/project/Book.java
@@ -5,4 +5,36 @@ public class Book {
     private String bookName;
     private String ISBN; 
     private int yearPub;
+
+    public Book(String bookName, String ISBN, int yearPub) {
+        this.bookName = bookName;
+        this.ISBN = ISBN;
+        this.yearPub = yearPub;
+    }
+    
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public int getYearPub() {
+        return yearPub;
+    }
+
+    public void setYearPub(int yearPub) {
+        this.yearPub = yearPub;
+    }
+    
+    
 }
